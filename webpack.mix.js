c60929778de96770fbe859b4eeeb9299bf4c06d2
RichardFuentes369 <javierbaron6@gmail.com>
2019-12-24 23:07:19 -0500

    navs

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,13 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css')
+   .browserSync({
+        proxy: 'http://parkingsmart.test/',
+        files: [
+            'app/**/*',
+            'resources/views/**/*',
+            'resources/lang/**/*',
+            'routes/**/*'
+        ]
+ });
